7371b67d471c7b1652bda06f3ded7b7c04ccfa91
David Jones <david.jones@deptagency.com>
2024-09-12 15:41:57 -0400

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const _RootElement = document.getElementById("LAZ_Widget");
+
+const returnProps = () => {
+  if (!_RootElement) {
+  }
+
+  return {
+    locationId: _RootElement.dataset.locationid,
+    arrive: _RootElement.dataset.arrive,
+    depart: _RootElement.dataset.depart,
+    header: _RootElement.dataset.header,
+    widgetKey: _RootElement.dataset.wk,
+    currentPage: _RootElement.dataset.currentpage,
+    eventDriven: _RootElement.dataset.eventdriven,
+    variant: _RootElement.dataset.variant,
+  };
+};
+
+returnProps();
+
+// style="height: 40%; padding-top: 2px;"
+const root = ReactDOM.createRoot(document.getElementById("LAZ_Widget"));
+
+root.render(<App {...returnProps()} />);
+
+// root.render(
+//   <React.StrictMode>
+//     <App {...returnProps()} />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
