d852086c54486145c2fef9903a6470f268d78467
David Jones <david.jones@deptagency.com>
2024-09-25 10:17:19 -0400

    more spacing

--- src/components/VariantSwitch.tsx
+++ src/components/VariantSwitch.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAppContext } from "../context";
 import MapWidget from "../variants/MapWidget";
 import BasicWidget from "../variants/BasicWidget";
+import { Box } from "@mui/material";
 
 const Variants = {
   basic: BasicWidget,
@@ -9,10 +10,16 @@ const Variants = {
 };
 
 const VariantSwitch = () => {
-  const { state: { variant = 'basic'} } = useAppContext();
-  const Component = Variants[variant as 'basic' | 'map'];
- 
-  return <Component />;
+  const {
+    state: { variant = "basic" },
+  } = useAppContext();
+  const Component = Variants[variant as "basic" | "map"];
+
+  return (
+    <Box padding={1}>
+      <Component />;
+    </Box>
+  );
 };
 
 export default VariantSwitch;
