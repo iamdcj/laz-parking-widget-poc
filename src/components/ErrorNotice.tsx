d6872b7ee6bba8fee80b805493dc7b138926eee0
David Jones <david.jones@deptagency.com>
2024-09-25 10:17:04 -0400

    spacing

--- src/components/ErrorNotice.tsx
+++ src/components/ErrorNotice.tsx
@@ -3,7 +3,7 @@ import { Alert } from "@mui/material";
 
 const ErrorNotice = ({ error }: { error: string }) => {
   return (
-    <Alert severity="error" variant="filled">
+    <Alert severity="error" variant="filled" sx={{ mb: 2 }}>
       {error}
     </Alert>
   );
