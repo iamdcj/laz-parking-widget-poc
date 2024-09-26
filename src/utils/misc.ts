64a14c6f92d011614c5ca8a823349fed06f4d3b3
David Jones <david.jones@deptagency.com>
2024-09-26 16:13:52 -0400

    this never ends

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -31,39 +31,50 @@ export interface Settings extends AppDefaults {
 
 export const returnInitialConfig = (element: HTMLElement): Settings => {
   const params = getUrlParam();
-  const modeOverwrite = !!element?.dataset?.modeOverwrite;
-  const modes = modeOverwrite ? element?.dataset?.mode : params.wt;
+  const modeOverwrite = !!element.dataset.modeOverwrite;
+  const modes = modeOverwrite ? element.dataset.mode : params.wt;
 
   return {
     modesOverride: modes?.split("/"),
-    widgetKey: params.wk || element?.dataset?.wk,
-    locationIds: params.l || element?.dataset?.locationid,
-    salesChannelKey: params.sc || element?.dataset?.sc,
-    agentId: params.aid || element?.dataset?.agentid,
-    hideEventDateTime: !!params.hed || !!element?.dataset?.hideEventDate,
+    useMap: element.dataset.map ? element.dataset.map === "true" : true,
+    isHeaderEnabled: element.dataset.header
+      ? element.dataset.header === "true"
+      : true,
+    widgetKey: params.wk || element.dataset.wk,
+    locationIds: params.l || element.dataset.locationid,
+    salesChannelKey: params.sc || element.dataset.sc,
+    agentId: params.aid || element.dataset.agentid,
+    hideEventDateTime:
+      element.dataset.hideEventDate === "true" || params.hed === "true",
     language: (params.isocode as "FR" | "EN") || "EN",
     evid: params.evid || null,
-    isHeaderEnabled: !!element?.dataset?.header,
-    headerText: element?.dataset?.headerText || null,
-    clientId: element?.dataset?.clientid || null,
-    useMap: !!element?.dataset?.map,
-    mapZoom: Number(element?.dataset?.mapzoom || 10),
-    mapLat: Number(element?.dataset?.mapplacelat || 0),
-    mapLng: Number(element?.dataset?.mapplacelng || 0),
-    eventDriven: !!element?.dataset?.eventdriven,
+    headerText: element.dataset.headerText || null,
+    clientId: element.dataset.clientid || null,
+    mapZoom: element.dataset.mapzoom ? Number(element.dataset.mapzoom) : 12,
+    mapLat: element.dataset.mapplacelat
+      ? Number(element.dataset.mapplacelat)
+      : 0,
+    mapLng: element.dataset.mapplacelng
+      ? Number(element.dataset.mapplacelng)
+      : 0,
+    eventDriven: element.dataset.eventdriven
+      ? params.wt === "evt"
+      : element.dataset.eventdriven === "true",
     // ---- TODO: determine the use cases for the following: //
-    startTime: params.start || element?.dataset?.starttime, // set the default start time of the widget (what is the format)
-    endTime: params.end || element?.dataset?.endtime, // set the default end time of the widget (what is the format)
-    arriveOffset: element?.dataset?.arrive
+    startTime: params.start || element.dataset.starttime, // set the default start time of the widget (what is the format)
+    endTime: params.end || element.dataset.endtime, // set the default end time of the widget (what is the format)
+    arriveOffset: element.dataset.arrive
       ? Number(element.dataset.arrive)
       : null, // offset in minutes (need use case)
-    departOffset: element?.dataset?.depart
+    departOffset: element.dataset.depart
       ? Number(element.dataset.depart)
       : null, // offset in minutes (need use case)
-    useFullWidget: !!element?.dataset?.fullwidget, // set whether or not to open the full widget url in a new window or to change the current url (example)
-    mapTxt: element?.dataset?.mapplacetxt || null, //! tbd
-    currentPage: !!element?.dataset?.currentpage, // Get the widget key (not sure about this one)
-    // template: element?.dataset?.template || null, // load template and stub style sheet based on iso language code (need examples)
+    useFullWidget: element.dataset.fullwidget
+      ? element.dataset.fullwidget === "true"
+      : false, // set whether or not to open the full widget url in a new window or to change the current url (example)
+    mapTxt: element.dataset.mapplacetxt || null, //! tbd
+    currentPage: !!element.dataset.currentpage, // Get the widget key (not sure about this one)
+    // template: element.dataset.template || null, // load template and stub style sheet based on iso language code (need examples)
   };
 };
 
